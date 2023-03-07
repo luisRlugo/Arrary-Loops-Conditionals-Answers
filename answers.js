91d8f6dbe5f71912d5a28eb2a763c65691354dcc
Luis <roberto_lugo95@yahoo.com>
2023-03-07 16:19:26 -0600

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -24,6 +24,24 @@ console.log("==============================");
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+console.log("==============================");
 
 ////////////////////////////////
 // Wild Wild Life
